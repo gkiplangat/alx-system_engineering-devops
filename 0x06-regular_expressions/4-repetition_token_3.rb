e195e4a80391f1e8bcba2156f875a58ba43ecb3a
gkiplangat <gideonkiplangat4@gmail.com>
2023-08-01 04:37:38 -0700

    feat:4-repetition_token_3.rb

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# Accept the argument from the command line
+input = ARGV[0]
+
+# Define the regular expression to match the cases
+regex = (/hbt*n/)
+
+# Use the regular expression to find matches
+matches = input.scan(regex)
+
+# Print the matches
+puts matches
