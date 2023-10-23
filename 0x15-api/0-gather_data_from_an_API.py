689c5df22acb8e8e3109492cdec0c274328f85a6
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2023-10-23 23:36:55 +0300

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" Rest API script that gathers data from an API. """
+import json
+import sys
+import urllib
+import urllib.request
+
+# The base API url for getting the employee object
+USER_API_URL = "https://jsonplaceholder.typicode.com/users/"
+
+# The base API url for getting all todo objects for an employee
+TODO_API_URL = "https://jsonplaceholder.typicode.com/todos?userId="
+
+# Employee ID passed as an argument to the script
+emp_id: str = sys.argv[1] if len(sys.argv) > 1 else ""
+
+# Get all the todos for a given employee ID
+if emp_id.isdigit():
+    try:
+        user_url = f"{USER_API_URL}{emp_id}"
+        todos_url = f"{TODO_API_URL}{emp_id}"
+
+        emp_resp = urllib.request.urlopen(user_url)
+        todos_resp = urllib.request.urlopen(todos_url)
+
+        emp_data = emp_resp.read()
+        todos_data = todos_resp.read()
+
+        employee_data = json.loads(emp_data)
+        todos = json.loads(todos_data)
+
+        name = employee_data.get("name")
+        done = len([todo for todo in todos if todo.get("completed")])
+        total = len(todos)
+
+        print(f"Employee {name} is done with tasks({done}/{total}):")
+        for todo in todos:
+            if todo.get("completed"):
+                print(f"\t {todo.get('title')}")
+
+    except urllib.error.URLError as err:
+        print(f"An error occurred: {err}")
+    except json.JSONDecodeError as err:
+        print(f"Error decoding JSON: {err}")
