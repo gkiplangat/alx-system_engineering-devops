2ee7d46ba955dea667b17234da2c1d7d2d651fb8
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2023-11-08 00:17:33 +0300

    feat:added

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Queries the Reddit API and returns the number of subscribers """
+import requests
+
+
+def number_of_subscribers(subreddit) -> int:
+    """
+    Queries the Reddit API and returns the number of subscribers
+
+    Args:
+        subreddit (str): subreddit to query
+
+    Returns:
+        (int): number of subscribers
+    """
+    # Returns the number of subscribers for a given subreddit
+    base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    try:
+        request = requests.get(base_url, headers={'User-Agent': 'Agent Gideon'})
+        data = request.json()
+        return data['data']['subscribers']
+    except Exception:
+        return 0
