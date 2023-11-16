09e3f085b11f36102a4be0dcb85feaf1ff18b77f
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2023-11-16 22:51:40 +0300

    feat:advance

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# Increase holberton user limit
+exec { 'increase-hard-file-limit':
+  command => 'sed -i "/holberton hard/s/5/8192/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/8192/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
